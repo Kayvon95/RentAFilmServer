22754ac72031bfc7ebfbed7bfe1dae70811a1def
Lars Smans <mhsmans@avans.nl>
2017-06-21 12:55:16 +0200

    fix voor de endpoint

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(expressJWT({
         { url: '/api/v1/login', methods: ['POST'] },
         { url: '/api/v1/register', methods: ['POST'] },
         { url: '/api/v1/films?offset=:start&count=:number', methods: ['GET'] },
+        { url: /\/api\/v1\/films/i, methods: ['GET'] },
         { url: '/api/v1/films/', methods: ['GET'] },
-        { url: '/api/v1/films/:filmid?', methods: ['GET'] }
     ]
 }));
 
